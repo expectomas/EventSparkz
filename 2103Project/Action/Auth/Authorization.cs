171ac8c3845019a60ed4085e06f6280924305168
Ethan Lim <expectomas@gmail.com>
2012-02-12 22:30:45 +0800

    Create Interface for Auth

---
+++ 2103Project/Action/Auth/Authorization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2103Project.Action
+{
+    interface Authorization
+    {
+        //Methods are automatically public
+              void sendUserNamePW(string i_UserName, string i_PW);
+              bool Authetication();
+    }
+}
