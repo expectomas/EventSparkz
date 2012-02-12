552dae61e8e338536da68f1ae669da3a01b8ddce
Ethan Lim <expectomas@gmail.com>
2012-02-12 22:29:58 +0800

    Temporary Stop Splash

--- 2103Project/Config/Program.cs
+++ 2103Project/Config/Program.cs
@@ -18,18 +18,20 @@ namespace _2103Project
 
             SplashImageForm f = new SplashImageForm();
 
-            f.Shown += new EventHandler((o, e) =>
-            {
-                System.Threading.Thread t = new System.Threading.Thread(() =>
-                {
-                    System.Threading.Thread.Sleep(2000);
-                    f.Invoke(new Action(() => { f.Close(); }));
+            //f.Shown += new EventHandler((o, e) =>
+            //{
+            //    System.Threading.Thread t = new System.Threading.Thread(() =>
+            //    {
+            //        System.Threading.Thread.Sleep(2000);
+            //        f.Invoke(new Action(() => { f.Close(); }));
 
-                });
-                t.IsBackground = true;
-                t.Start();
-            });
-            Application.Run(f);
+            //    });
+            //    t.IsBackground = true;
+            //    t.Start();
+            //});
+
+            // Application.Run(f);
+
             Application.Run(new loginForm());
         }
     }
