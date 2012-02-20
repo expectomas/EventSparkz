df2f3e6f010a4fcfd428c37ab68244c99b8bbd39
williamtio <william_may_cry_x@hotmail.com>
2012-02-20 11:58:09 +0800

    Updated: Messagebox issue

--- 2103Project/cancelEvent.cs
+++ 2103Project/cancelEvent.cs
@@ -24,49 +24,56 @@ namespace _2103Project
 
         private void confirmButton_Click(object sender, EventArgs e)
         {
-            int index = eventComboBox.SelectedIndex;
-            string path = "event.txt";
-            string tempPath = "temp.txt";
-            DialogResult result;
-            result = MessageBox.Show("Delete this event?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
-            if (result == DialogResult.OK)
+            if (eventComboBox.SelectedItem == null)
             {
-                string strIndex = index.ToString();
-                StreamReader sr = new StreamReader(path);
-                using (StreamWriter swtemp = new StreamWriter(tempPath))
+                MessageBox.Show("Please choose an event.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);                
+            }
+            else
+            {
+                int index = eventComboBox.SelectedIndex;
+                string path = "event.txt";
+                string tempPath = "temp.txt";
+                DialogResult result;
+                result = MessageBox.Show("Delete this event?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
                 {
-                    while (sr.Peek() >= 0)
+                    string strIndex = index.ToString();
+                    StreamReader sr = new StreamReader(path);
+                    using (StreamWriter swtemp = new StreamWriter(tempPath))
                     {
-                        string readLineValue = sr.ReadLine();
-                        if (readLineValue.Equals(strIndex))
+                        while (sr.Peek() >= 0)
                         {
-                            for (int i = 0; i < 5; i++)
+                            string readLineValue = sr.ReadLine();
+                            if (readLineValue.Equals(strIndex))
                             {
-                                sr.ReadLine();
+                                for (int i = 0; i < 5; i++)
+                                {
+                                    sr.ReadLine();
+                                }
+                            }
+                            else
+                            {
+                                swtemp.WriteLine(readLineValue);
                             }
                         }
-                        else
-                        {
-                            swtemp.WriteLine(readLineValue);
-                        }
+                        swtemp.Close();
+                        sr.Close();
                     }
-                    swtemp.Close();
-                    sr.Close();
-                }
-                StreamReader sr2 = new StreamReader(tempPath);
-                using (StreamWriter sw2 = new StreamWriter(path))
-                {
-                    while (sr2.Peek() >= 0)
+                    StreamReader sr2 = new StreamReader(tempPath);
+                    using (StreamWriter sw2 = new StreamWriter(path))
                     {
-                        string readLineValue = sr2.ReadLine();
-                        sw2.WriteLine(readLineValue);
+                        while (sr2.Peek() >= 0)
+                        {
+                            string readLineValue = sr2.ReadLine();
+                            sw2.WriteLine(readLineValue);
+                        }
+                        sr2.Close();
+                        sw2.Close();
                     }
-                    sr2.Close();
-                    sw2.Close();
+                    eventComboBox.Items.Remove(eventComboBox.SelectedItem);
+                    MessageBox.Show("You have deleted this event", "Delete Event", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-                eventComboBox.Items.Remove(eventComboBox.SelectedItem);
-                MessageBox.Show("You have deleted this event", "Delete Event", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            }   
         }
 
         private void cancelEventForm_Load(object sender, EventArgs e)
