8cbf88cad8c7e1b094bf0ad3b758b5dfa0d641e7
Ethan Lim <expectomas@gmail.com>
2012-02-28 11:50:05 +0800

    Remove LINQ statements -> implemented standard wait before splash screen

--- 2103Project/Config/Program.cs
+++ 2103Project/Config/Program.cs
@@ -18,19 +18,13 @@ namespace _2103Project
 
             SplashImageForm f = new SplashImageForm();
 
-            f.Shown += new EventHandler((o, e) =>
-            {
-                System.Threading.Thread t = new System.Threading.Thread(() =>
-                {
-                    System.Threading.Thread.Sleep(2000);
-                    f.Invoke(new Action(() => { f.Close(); }));
-
-                });
-                t.IsBackground = true;
-                t.Start();
-            });
+            
 
             Application.Run(f);
+
+            System.Threading.Thread.Sleep(2000);
+
+            f.Close();
 
             Application.Run(new loginForm());
         }
