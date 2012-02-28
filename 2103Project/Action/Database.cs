3a602f586f707a37184a2279b95526d622eaeddd
Ethan Lim <expectomas@gmail.com>
2012-02-28 13:34:10 +0800

    Database + Database Interface

---
+++ 2103Project/Action/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2103Project.Action
+{
+    class Database : DatabaseInterface
+    {
+    }
+}
