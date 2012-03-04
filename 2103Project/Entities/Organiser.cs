082ac884da60035bc8be2b603afab6251f45c23a
williamtio <william_may_cry_x@hotmail.com>
2012-03-04 10:19:01 +0800

    Updated: Create and cancel event feature

--- 2103Project/Entities/Organiser.cs
+++ 2103Project/Entities/Organiser.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using _2103Project.Action;
 
 namespace _2103Project.Entities
 {
@@ -9,26 +11,110 @@ namespace _2103Project.Entities
     {
         //Attributes
         public List<EventEntity> viewBookings;
+        private int eventId;
+        private string eventName;
+        private DateTime startTime;
+        private DateTime endTime;
+        private int eventScheduleId;
+        private int participantSize;
+        private List<Participant> participantList;
 
-         //Database Access Authetication
+        // Constructor
+        public Organiser() { }
+
+        public Organiser(int i_eventId, string i_eventName, DateTime i_startTime, DateTime i_endTime, int i_eventScheduleId, int i_participationSize, List<Participant> I_participantList)
+        {
+            eventId = i_eventId;
+            eventName = i_eventName;
+            startTime = i_startTime;
+            endTime = i_endTime;
+            eventScheduleId = i_eventScheduleId;
+            participantSize = i_participationSize;
+            participantList = I_participantList;
+        }
+        //Database Access Authetication
         private const string DatabaseToken = "9032!ds$";
-        
+
         //Methods
         public bool createEvent()
         {
-            bool eventCreated = false;
+            string path = "event.txt";
+            string tempPath = "temp.txt";
 
-
-            return eventCreated;
+            if (!File.Exists(path))
+            {
+                FileStream fs = File.Create(path);
+                fs.Close();
+            }
+            if (!File.Exists(tempPath))
+            {
+                FileStream fs = File.Create(tempPath);
+                fs.Close();
+            }
+            Database db = Database.CreateDatabase(DatabaseToken);
+            db.createEvent(eventId, eventName, startTime, endTime, eventScheduleId, participantSize);
+            return true;
         }
 
-        public bool cancelEvent()
+        public static int getLastEventId()
+        {
+            int count = 6;
+            string strid = "1";
+            StreamReader sr = new StreamReader("event.txt");
+            while (sr.Peek() >= 0)
+            {
+                if (count == 6)
+                {
+                    strid = sr.ReadLine();
+                    count = 1;
+                }
+                else
+                {
+                    sr.ReadLine();
+                    count++;
+                }
+            }
+            sr.Close();
+            return int.Parse(strid);
+        }
+
+        public static int getLastScheduleId()
+        {
+            int count = 2;
+            string strschid = "1";
+            StreamReader sr = new StreamReader("event.txt");
+            while (sr.Peek() >= 0)
+            {
+                if (count == 6)
+                {
+                    strschid = sr.ReadLine();
+                    count = 1;
+                }
+                else
+                {
+                    sr.ReadLine();
+                    count++;
+                }                    
+            }
+            sr.Close();
+            return int.Parse(strschid);
+        }
+
+        public bool cancelEvent(string selectedEventName)
         {
             bool eventCancelled = false;
 
+            Database db = Database.CreateDatabase(DatabaseToken);
+            db.deleteEvent(selectedEventName);
+            return !eventCancelled;
+        }
 
-
-            return eventCancelled;
+        public static Queue<string> loadListOfEvent()
+        {
+            Database db = Database.CreateDatabase(DatabaseToken);
+            Queue<string> listOfEvents = new Queue<string>();
+            db.loadListOfEvent(listOfEvents);
+            return listOfEvents;
         }
 
         public bool updateEvent()
@@ -38,6 +124,6 @@ namespace _2103Project.Entities
 
             return eventUpdated;
         }
-       
+
     }
 }
