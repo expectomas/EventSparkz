49e142617824a96c2d854eed8c31edd8b943a1a8
williamtio <william_may_cry_x@hotmail.com>
2012-03-09 08:11:52 +0800

    Fixed: 0 argument before enter new form

--- 2103Project/EventSparkz.cs
+++ 2103Project/EventSparkz.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using _2103Project.Entities;
 
 namespace _2103Project
 {
@@ -19,7 +18,7 @@ namespace _2103Project
 
         private void loginButton_Click(object sender, EventArgs e)
         {
-            loginForm login = new loginForm(new User());
+            loginForm login = new loginForm(null);
             login.Show();
         }
 
