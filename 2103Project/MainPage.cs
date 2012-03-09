cee5b7deb9dcba073b7599b430e82b05e60dba05
Ethan Lim <expectomas@gmail.com>
2012-03-09 12:19:38 +0800

    Changed Name

---
+++ 2103Project/MainPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using _2103Project.Entities;
+namespace _2103Project
+{
+    public partial class mainPage : Form
+    {
+        private User currentUser;
+        public mainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
