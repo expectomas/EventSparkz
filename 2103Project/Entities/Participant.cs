9a326fe1336d4bc3b7909c7e7edb2eceea5fe2cc
Ethan Lim <expectomas@gmail.com>
2012-03-11 23:21:34 +0800

    inserted names

--- 2103Project/Entities/Participant.cs
+++ 2103Project/Entities/Participant.cs
@@ -4,6 +4,14 @@ using System.Linq;
 using System.Text;
 using _2103Project.Action;
 
+/*
+ * Authors for this section
+ * 
+ *   Lim Zhi Hao A0067252H
+ *   Tio Wee Leong A0073702M
+ * 
+ */
+
 namespace _2103Project.Entities
 {
     class Participant : ActiveUser
