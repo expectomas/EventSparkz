b62bdf9c53f1e3b057c24d681acca593a01a9a2e
Ethan Lim <expectomas@gmail.com>
2012-03-22 22:57:21 +0800

    Advertisement Class

---
+++ 2103Project/Entities/Advertisement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2103Project.Entities
+{
+    class Advertisement
+    {
+        int advertisementID;
+        string imageDirectory;
+        string description;
+
+        public Advertisement(int newAdvertisementId, string newImageDirectory, string newDescription)
+        {
+            advertisementID = newAdvertisementId;
+            imageDirectory = newImageDirectory;
+            description = newDescription;
+        }
+    }
+}
