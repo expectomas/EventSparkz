ee4b6a51d182e346e341d52e19d24f7e15720045
Ethan Lim <expectomas@gmail.com>
2012-03-26 19:56:48 +0800

    new Budget Entity

---
+++ 2103Project/Entities/Budget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2103Project.Entities
+{
+    class Budget
+    {
+        private int itemID;
+        private double itemPrice;
+        private string itemName;
+    }
+}
