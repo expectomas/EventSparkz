6ce90a91b88d8f9c09d7ebfb2fbb1e835b310aa1
Ethan Lim <expectomas@gmail.com>
2012-03-27 12:20:56 +0800

    inserted the budget operation

--- 2103Project/Entities/Budget.cs
+++ 2103Project/Entities/Budget.cs
@@ -11,11 +11,18 @@ namespace _2103Project.Entities
         private double itemPrice;
         private string itemName;
 
-        Budget(int i_itemId, double i_itemPrice, string i_itemName)
+        public Budget(int i_itemId, double i_itemPrice, string i_itemName)
         {
             itemId = i_itemId;
             itemPrice = i_itemPrice;
             itemName = i_itemName;
+        }
+
+        public void requestBudgetDetails(ref int o_itemId, ref double o_itemPrice, ref string o_itemName)
+        {
+            o_itemId = itemId;
+            o_itemPrice = itemPrice;
+            o_itemName = itemName;
         }
     }
 }
