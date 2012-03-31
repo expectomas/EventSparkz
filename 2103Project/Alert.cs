13ddee6ffa2b38a27315c919403a1b177bbb601d
Edmund Tan <edmundtan@nus.edu.sg>
2012-04-01 01:43:19 +0800

    Alert Form

---
+++ 2103Project/Alert.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using _2103Project.Entities;
+
+namespace _2103Project
+{
+    public partial class AlertForm : Form
+    {
+        private User currentUser;
+
+        public AlertForm(User currentUser)
+        {
+            this.currentUser = currentUser;
+            InitializeComponent();
+        }
+
+        private void AlertForm_Load(object sender, EventArgs e)
+        {
+            initAlertList();
+            displayAlertList();        
+        }
+
+        private void initAlertList()
+        {
+            this.alertListView.Hide();
+
+            //Clear List
+            this.alertListView.Columns.Clear();
+            this.alertListView.Items.Clear();
+
+            this.alertListView.Columns.Insert(0, "No.", 30);
+            this.alertListView.Columns.Insert(1, "Event Name", 130);
+            this.alertListView.Columns.Insert(2, "Details", 185);
+
+            this.alertListView.Show();
+        }
+
+        private void displayAlertList()
+        {
+            //Clear List
+            alertListView.Items.Clear();
+
+            List<Alert> alerts = new List<Alert>();
+            EventEntity eve = new EventEntity();
+            ActiveUser au = new ActiveUser(currentUser);
+            alerts = au.getListOfAlerts();
+            int currentEventID;
+
+            if (alerts.Count != 0)
+            {
+                for (int i = 0; i < alerts.Count; i++)
+                {
+                    ListViewItem newAlert = new ListViewItem((i + 1).ToString());
+                    newAlert.SubItems.Add(alerts[i].getAlertedEventName()); // Name of Event
+                    newAlert.SubItems.Add(alerts[i].getAlert()); // Alert String
+                    alertListView.Items.Add(newAlert);
+                    currentEventID = eve.getEventIDFromEventName(alerts[i].getAlertedEventName()); // Clear Alert Flag after Displaying
+                    eve.clearEventUpdatedFlag(currentEventID);
+                }
+            }
+
+            alertListView.Show();
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            this.alertListView.Items.Clear();
+            this.Close();
+        }
+    }
+}
---
+++ 2103Project/Alert.designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _2103Project
+{
+    partial class AlertForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.alertListView = new System.Windows.Forms.ListView();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // alertListView
+            // 
+            this.alertListView.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alertListView.GridLines = true;
+            this.alertListView.Location = new System.Drawing.Point(12, 12);
+            this.alertListView.Name = "alertListView";
+            this.alertListView.Size = new System.Drawing.Size(345, 289);
+            this.alertListView.TabIndex = 0;
+            this.alertListView.UseCompatibleStateImageBehavior = false;
+            this.alertListView.View = System.Windows.Forms.View.Details;
+            // 
+            // okBtn
+            // 
+            this.okBtn.Location = new System.Drawing.Point(146, 307);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.TabIndex = 1;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // AlertForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 335);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.alertListView);
+            this.Name = "AlertForm";
+            this.Text = "Alert";
+            this.Load += new System.EventHandler(this.AlertForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView alertListView;
+        private System.Windows.Forms.Button okBtn;
+
+    }
+}
