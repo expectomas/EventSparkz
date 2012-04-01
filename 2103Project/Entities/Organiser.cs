dfceccf9d6c7e9c871502246c6018895e2e43153
Ethan Lim <expectomas@gmail.com>
2012-04-01 22:23:21 +0800

    changed get event

--- 2103Project/Entities/Organiser.cs
+++ 2103Project/Entities/Organiser.cs
@@ -254,7 +254,7 @@ namespace _2103Project.Entities
 
             foreach (EventEntity eventCreated in createdEvents)
             {
-                if(EventEntity.getParticipantNumber(eventCreated.getEventId())>participantCount))
+                if(EventEntity.getParticipantNumber(eventCreated.getEventId())>participantCount)
                 {
                     participantCount = eventCreated.getParticipantSize();
 
