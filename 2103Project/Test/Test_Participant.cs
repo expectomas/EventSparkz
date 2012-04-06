6e8ae0b0bfc07699846dbe054d44ccb7c8375ca5
Desmond Wee <alwee89@hotmail.com>
2012-04-06 13:46:48 +0800

    edited test participant

--- 2103Project/Test/Test_Participant.cs
+++ 2103Project/Test/Test_Participant.cs
@@ -2,24 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using _2103Project.Action;
 using _2103Project.Entities;
 
 namespace _2103test._2103Project.Test
 {
     class Test_Participant
     {
-        public bool Test_registerEvent(Participant testParticipant, EventEntity testEvent )
+        public bool Test_registerEvent()
         {
             bool testSuccess = false;
+            const string DatabaseToken = "ewknf%32";
 
-            Participant target = testParticipant;
-            EventEntity interestedEvent = testEvent;
-            bool expected = true;
-            bool actual;
-            actual = target.registerEvent(interestedEvent);
-            //assert
-            if (expected == actual)
-                testSuccess = true;
+            Database db = Database.CreateDatabase(DatabaseToken);
+            List<User> userlist = db.getListOfUsers();
+            User theuser = userlist[1];
+            Participant target = new Participant(theuser);
+
+            db = Database.CreateDatabase(DatabaseToken);
+            List<EventEntity> eventlist = db.getListOfEvents();
+            EventEntity theEvent = eventlist[1];
+            theEvent.setParticipantSize(50);
+            if(EventEntity.getParticipantNumber(theEvent.getEventId())==49)
+            {
+                bool expected = true;
+                bool actual;
+                actual = target.registerEvent(theEvent);
+                //assert
+                if (expected == actual)
+                    testSuccess = true;
+                return testSuccess;
+            }
+            else
+            {
+                if (EventEntity.getParticipantNumber(theEvent.getEventId()) == 50)
+                {
+                    bool expected = false;
+                    bool actual;
+                    actual = target.registerEvent(theEvent);
+                    //assert
+                    if (expected == actual)
+                        testSuccess = true;
+                    return testSuccess;
+                }
+            }
 
             return testSuccess;
         }
